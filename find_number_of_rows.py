ad49eea4dfe19b899b3c5286ea100afb299cb76b
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2022-12-20 21:30:11 +0500

    find_number_of_rows.py

--- find_number_of_rows.py
+++ find_number_of_rows.py
@@ -6,6 +6,10 @@ def find_number_of_rows(data):
     Return:
         int: Number of rows.
     """
-    return
+    data = data.split('\n')
+    return len(data) - 1
 
 # Read the csv file
+f = open('data.csv')
+a = f.read()
+print(find_number_of_rows(a))
